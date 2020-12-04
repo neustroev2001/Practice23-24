08fa41357c7c1d1ed21e6244bf75b788f1ab8c57
Leonid <neustroev.leonid@mail.ru>
2020-12-04 15:54:27 +0300

    Ex.23-24

---
+++ src/task23_24/Transaction.java
@@ -0,0 +1,18 @@
+package task23_24;
+
+public class Transaction {
+
+    String paymentDate;
+    double amountplus;
+    double amountminus;
+    String reference;
+    String accountBic;
+    String accountName;
+    String currency;
+    String info;
+
+    public Transaction() {
+        this.amountplus = 0;
+        this.amountminus=0;
+    }
+}
---
+++ src/task23_24/main22_23.java
@@ -0,0 +1,14 @@
+package task23_24;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.IOException;
+
+public class main22_23 {
+
+        public static void main(String[] args) throws IOException, CsvException {
+                //Bankcard bankcard=new Bankcard();
+                //bankcard.Readingfile();
+                Images images= new Images();
+                images.Finding();
+        }
+}
